235249ff7c197cdc502983f883fa8ba44859af30
Anubhav Gupta <anubhav.gupta@digi.com>
2022-11-18 15:21:29 -0500

    netifd: bridge and wifi related changes
    1. In rare cases, the bridge asserts if the wlan macaddr changes
       If the wlan macaddr changes (eg. from the default to the configured
       value) whilst being added to the bridge, it would assert. This was the
       only assert left in the bridge code from the original 2011 creation. All
       other routines simply checked and returned. We now do this.
       Tested on an EX15W and IX20W
    
    2. bridge: the bridge mac address is now the first on the list (if up).
       If the first isn't available, the the first available interface is used.
       When a volatile interface (eg wifi) comes up, it will reload the bridge
       and change the macaddress to the new address.
    
    DAL-3949

--- bridge.c
+++ bridge.c
@@ -360,8 +360,9 @@ bridge_enable_member(struct bridge_member *bm)
 	struct device *dev;
 	int ret;
 
-	if (!bm->present)
-		return 0;
+	/* as we only add devices to the uci file that are enabled,
+		ignore the present indicator. */
+	bm->present = true;
 
 	ret = bridge_enable_interface(bst);
 	if (ret)
@@ -526,7 +527,8 @@ bridge_member_cb(struct device_user *dep, enum device_event ev)
 
 	switch (ev) {
 	case DEV_EVENT_ADD:
-		assert(!bm->present);
+		if (bm->present)
+			return;
 
 		bm->present = true;
 		bst->n_present++;
@@ -661,8 +663,10 @@ bridge_create_member(struct bridge_state *bst, const char *name,
 	 * already existed
 	 */
 	bm = vlist_find(&bst->members, name, bm, node);
-	if (hotplug && bm)
+	if (hotplug && bm) {
 		bm->node.version = -1;
+		bm->present = true;
+	}
 
 	return bm;
 }
