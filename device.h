4dfd61055e1c51d6e6ffb0b82acc12273b2267a5
Anubhav Gupta <anubhav.gupta@digi.com>
2022-11-28 16:46:49 -0500

    netifd: Support for configurable default packet steering behavior
    The default packet steering behavior can be configured via the parameter
    default_ps in the global section; the default value is true to keep
    backwards compatibility.
    Device packet steering (rps/xps) config can still be used to override the
    default behavior.
    This allows you to disable packet steering for all devices without the need
    to define a device config list which disables receive/transmit packet steering

--- device.h
+++ device.h
@@ -40,6 +40,8 @@ enum {
 	DEV_ATTR_IGMPVERSION,
 	DEV_ATTR_MLDVERSION,
 	DEV_ATTR_NEIGHREACHABLETIME,
+	DEV_ATTR_RPS,
+	DEV_ATTR_XPS,
 	DEV_ATTR_DADTRANSMITS,
 	DEV_ATTR_MULTICAST_TO_UNICAST,
 	DEV_ATTR_MULTICAST_ROUTER,
@@ -91,7 +93,8 @@ enum {
 	DEV_OPT_IGMPVERSION		= (1 << 7),
 	DEV_OPT_MLDVERSION		= (1 << 8),
 	DEV_OPT_NEIGHREACHABLETIME	= (1 << 9),
-	/* 2 bit hole */
+	DEV_OPT_RPS			= (1 << 10),
+	DEV_OPT_XPS			= (1 << 11),
 	DEV_OPT_MTU6			= (1 << 12),
 	DEV_OPT_DADTRANSMITS		= (1 << 13),
 	DEV_OPT_MULTICAST_TO_UNICAST	= (1 << 14),
@@ -162,6 +165,8 @@ struct device_settings {
 	unsigned int neigh4gcstaletime;
 	unsigned int neigh6gcstaletime;
 	int neigh4locktime;
+	bool rps;
+	bool xps;
 	unsigned int dadtransmits;
 	bool multicast_to_unicast;
 	unsigned int multicast_router;
@@ -249,6 +254,7 @@ device_apply_config(struct device *dev, struct device_type *type,
 
 void device_reset_config(void);
 void device_reset_old(void);
+void device_set_default_ps(bool state);
 
 void device_init_virtual(struct device *dev, struct device_type *type, const char *name);
 int device_init(struct device *dev, struct device_type *type, const char *ifname);
